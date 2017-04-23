a54d407125ffe0bce11c6928013a4bd58b2e88dd
karam <karammann98@gmail.com>
2017-04-23 23:09:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/karam/admin_login_notes_sharing/admin_login.java
@@ -0,0 +1,13 @@
+package com.example.karam.admin_login_notes_sharing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class admin_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
